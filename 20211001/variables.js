f91e8704a8f6ae89b5aca128ef88f59e4cdc8b54
erik <emendezmartin@iessonferrer.net>
2021-10-04 16:36:26 +0200

    asd

--- 20211001/variables.js
+++ 20211001/variables.js
@@ -1,5 +1,19 @@
-//Variables
+//Variables definir variables con 'var' y 'let'
 
-var x=9;
-
+var x;
+x=10;
 console.log(x);
+
+let y=20;
+console.log(y);
+
+z = x+y;
+console.log(z);
+
+//Constantes definir constantes con 'const'
+
+const PI=3.1416;
+console.log(PI);
+
+PI=6.28//Esto dara un error ya que es una constante 
+//y no se puede cambiar el valor
