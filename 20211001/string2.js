37663d9545109c6593a53836e723ee83d15ab0c5
erik <emendezmartin@iessonferrer.net>
2021-10-05 16:35:37 +0200

    MASTER

---
+++ 20211001/string2.js
@@ -0,0 +1,8 @@
+// para mostrarlo por consola deberas poner 'node (nombre del archivo).js'
+var nombre1="EriKoTe";
+var nombre2='PepinoMaster69XD';
+var nombre3= `Belen`;
+
+console.log ("Mi nombre es nombre1");
+console.log("Mi nombre es "+ nombre2);
+console.log(`Mi nombre es ${nombre2}`;
--- 20211001/variables.js
+++ 20211001/variables.js
@@ -1,13 +1,14 @@
 //Variables definir variables con 'var' y 'let'
-
+// para mostrarlo por consola deberas poner 'node (nombre del archivo).js'
 var x;
 x=10;
 console.log(x);
-
+console.log("+");
 let y=20;
 console.log(y);
 
 z = x+y;
+console.log("=");
 console.log(z);
 
 //Constantes definir constantes con 'const'
