6a8f14da3adbca16f05361319bae678d193d687e
erik <emendezmartin@iessonferrer.net>
2021-10-08 17:53:21 +0200

    master

---
+++ Activitats1_JS/Ejercicio3.js
@@ -0,0 +1,8 @@
+
+let nombreCompleto =prompt("Escribe tu nombre completo: ");
+
+let salario = prompt("Digame cuanto cobra: ");
+
+let edad = prompt ("Cual es su edad: ");
+
+alert(`${nombreCompleto}`),`${salario}`;
