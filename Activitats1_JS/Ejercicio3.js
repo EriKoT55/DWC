c38e79ab4df71a23a1e63fb0ff61d8deaaa8a0ba
EriKoT <erik55@outlook.es>
2021-10-10 19:30:24 +0200

    master

--- Activitats1_JS/Ejercicio3.js
+++ Activitats1_JS/Ejercicio3.js
@@ -1,8 +1,30 @@
 
 let nombreCompleto =prompt("Escribe tu nombre completo: ");
 
-let salario = prompt("Digame cuanto cobra: ");
+let salario = parseInt(prompt("Digame cuanto cobra: "));
 
-let edad = prompt ("Cual es su edad: ");
+let edad = parseInt(prompt ("Cual es su edad: "));
 
-alert(`${nombreCompleto}`),`${salario}`;
+
+
+if(salario>=1000 && salario<=2000){
+
+    if(edad>45){
+        salario=salario+salario*0.03;
+    }else if(edad<=45){
+        salario=salario+(salario*0.1);
+    }
+
+}else if(salario<1000){
+
+    if(edad<30){
+        salario=1100;
+    }else if(edad>=30 && edad<=45){
+        salario = salario+salario*0.03;
+    }else if(edad>45){
+        salario=salario+salario*0.15;
+    }
+
+}
+
+alert(`${nombreCompleto}`+` `+`${edad}`+` `+`${salario}`);
---
+++ Activitats1_JS/Ejercicio4.js
@@ -0,0 +1,108 @@
+
+
+var random = Math.floor(Math.random()*101);
+
+var numIntentos=0;
+let isNum=false;
+let acertado=false;
+
+let volver= confirm("¿Te gustaria jugar?");
+var num=prompt("Adivine el número: ");
+
+
+while(volver==true){
+    
+    random = Math.floor(Math.random()*101);
+    alert(random);
+    num=prompt("Adivine el número: ");
+    
+    while(isNum==false){
+        if(isNaN(num)){
+            alert("No es un número");   
+            num=prompt("Ponga un número, por favor: ");
+            isNum=false;
+        }
+        else if(!isNaN(num)){
+            if(num==null){
+                alert("Se cerro el juego");
+            }else{
+                while(acertado==false){
+           
+                    if(num==random){
+                        alert("Has acertadoo");
+                        acertado=true;
+                    }else{
+            
+                        if(num<random){
+                            num=prompt("El número introducido es menor al número que busca, intente lo otra vez: ")
+                            acertado=false;
+                            if(num==null){
+                                alert("Se cerro el juego");
+                                acertado=true;
+                            }
+                        }else if(num>random){
+                
+                            num=prompt("El número introducido es mayor al número que busca, intente lo otra vez: ")
+                            acertado=false;
+                            if(num==null){
+                                alert("Se cerro el juego");
+                                acertado=true;
+                            }
+                        }
+                        numIntentos++;
+                    }
+                }
+            }
+            isNum=true;
+        }
+        
+            alert(`Intentos: ${numIntentos} `);
+        
+    }
+    volver=confirm("¿Te gustaria vovler a jugar?");
+    if(volver==false){
+     alert("Bueno en otra ocasión sera");
+    }
+    
+}
+
+
+
+ /*if(num==null){
+        alert("Se cerro el juego");
+   }else{
+        while(acertado==false){
+           
+         if(num==random){
+            alert("Has acertadoo");
+            acertado=true;
+         }else{
+            
+            if(num<random ){
+                num=prompt("El número introducido es menor al número que busca, intente lo otra vez: ")
+                acertado=false;
+                if(num==null){
+                    alert("Se cerro el juego");
+                    acertado=true;
+                }
+            }else if(num>random){
+                
+                num=prompt("El número introducido es mayor al número que busca, intente lo otra vez: ")
+                acertado=false;
+                if(num==null){
+                    alert("Se cerro el juego");
+                    acertado=true;
+                }
+            }
+            numIntentos++;
+            
+         }
+            
+         
+        }
+        
+     
+    }
+    alert(`Intentos: ${numIntentos} `);
+
+ */  
