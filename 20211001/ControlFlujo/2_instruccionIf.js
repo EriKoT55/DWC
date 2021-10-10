6e5cd86e8a687d92e969b04460e1679ebb997934
EriKoT <erik55@outlook.es>
2021-10-10 19:46:49 +0200

    master

--- 20211001/ControlFlujo/2_instruccionIf.js
+++
@@ -1,8 +0,0 @@
-/* INSTRUCCIÓN IF */
-//Sentencia condicional simple
-var nota = 3;
-var aprobados = 0;
-if(nota >= 5){
-    console.log("Aprobado");
-    aprobados++;
-}
--- 20211001/ControlFlujo/5_instruccionWhile.js
+++
@@ -1,7 +0,0 @@
-/* INSTRUCCIÓN WHILE */
-//La condición se comprueba antes de entrar al while
-var i = 1;
-while (i <= 100){
-    console.log(i);
-    i++
-}
