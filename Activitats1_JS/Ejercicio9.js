9ce67d645d1ea226810c9dee4c6ac1b2ffacca9b
erik <emendezmartin@iessonferrer.net>
2021-10-18 19:53:43 +0200

    master

---
+++ Activitats1_JS/Ejercicio9.js
@@ -0,0 +1,17 @@
+var red;
+var verde;
+var azul;
+var izq;
+var arriba;
+
+for(let i=1;i<2000;i++){
+    
+    rojo=parseInt(Math.random()*256);
+    verde= parseInt(Math.random()*256);
+    azul=parseInt(Math.random()*256);
+    altura=parseInt(Math.random()*100);
+    izq=parseInt(Math.random()*100);
+
+    <div id='fondo'></div>
+
+}
