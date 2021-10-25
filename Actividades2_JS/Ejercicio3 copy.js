a692f7bde980962f3370c331f48a0864e373b170
erik <emendezmartin@iessonferrer.net>
2021-10-25 16:32:04 +0200

    g

--- Actividades2_JS/Ejercicio3 copy.js
+++ Actividades2_JS/Ejercicio3 copy.js
@@ -1,28 +1,26 @@
 
 let random;
-
+const randomizer = new Set();
 
 for(let j=1;j<=50;j++){
 
     
-    for(let i=0;i<6;i++){
-        random = Math.floor(Math.random()*51+1);
+    do{
+        randomizer.add(Math.floor(Math.random()*50+1));
+    }while(randomizer.size<6);
 
-        const randomizer = new Set([random]);// array de random sin repetirse
-        
+    document.write("<br>");
     
-
-    // no acabo de entender como utilizar randoms.has como condicion 
+    for(numeros of randomizer){
+        
+        document.write(" "+numeros);
+    }
+    randomizer.clear();
+}
+// no acabo de entender como utilizar randoms.has como condicion 
     // me devuelve un booleado, true si se repite, false si no
     // lo que no acabo de entender es si me da false, que es lo que quiero, 
     // el problema que set guarda los numeros y claro guarda los 50 random entoces
     // cuando vaya a salir cualquier numero random, ya no se mostrara, por que seria repetido
     // lo que quiero hacecr es que en la linea no se repita, pero no acabo de ver como hacer lo  
     
-
-            document.write(" "+random);
-            
-           
-        
-        }
-}
