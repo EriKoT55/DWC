69af35686841496f06be9eb6292777fb72ca29fd
erik <emendezmartin@iessonferrer.net>
2021-10-27 21:00:51 +0200

    27 oct 2021

--- Actividades2_JS/Ejercicio5.js
+++ Actividades2_JS/Ejercicio5.js
@@ -1,7 +1,7 @@
 //Arrays
 let arrayMayus=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z"];
 let arrayMinus=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"];
-let arrayNums=[0,1,2,3,4,5,6,7,8,9];
+let arrayNums=["0","1","2","3","4","5","6","7","8","9"];
 
 //Booleano
 let boolean=false;
@@ -10,23 +10,23 @@ let nom=prompt("Ponga su nombre de usuario: (Debe tener minusculas y numeros)");
 
 let arrayChars=nom.split("");
 
-for(let j=0;j<arrayMayus.length;j++){
 
-    
+
     for(let i=0;i<arrayChars.length;i++){
 
-        console.log(arrayMayus.indexOf(arrayChars[i]));
-        
-        /*if(arrayMayus.indexOf(arrayChars)==arrayChars[i]){
-            alert("encontramos una mayus");
-            nom=prompt("Ponga su nombre de usuario:(Debe tener minusculas y numeros)");
-        }else{
-            alert("no esta");
+        if( arrayMayus.indexOf(arrayChars[i]) != -1 ){
+
+            alert("He encontrado un mayus");
+  
+        }if( arrayNums.indexOf(arrayChars[i]) == -1){
+
+                    alert("No he encontrado un numeo");
+                
         }
-         */
-    }
+            nom=prompt("Ponga su nombre de usuario:(Debe tener minusculas y numeros)");
+            
+        }
     
-}
 
 
 
