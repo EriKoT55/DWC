42615c4b80d4335c52d1e1cfe42373fb803e99d9
EriKoT <erik55@outlook.es>
2021-10-28 15:43:29 +0200

    28 OCT 2021

--- Actividades2_JS/Ejercicio5.js
+++ Actividades2_JS/Ejercicio5.js
@@ -3,32 +3,75 @@ let arrayMayus=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
 let arrayMinus=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"];
 let arrayNums=["0","1","2","3","4","5","6","7","8","9"];
 
-//Booleano
-let boolean=false;
+//Arrays de Resultados
+let resultNumsIndexOf=[];
+let resultMayusIndexOf=[];
+
+//Booleanos condiciones
+let booleanMayus=false;
+let booleanMinus=false;
+let booleanNums=false;
+
+//Booleanos bucle
+let booleanWhile=false;
+
+//Contador
+let contador=0;
+
 
 let nom=prompt("Ponga su nombre de usuario: (Debe tener minusculas y numeros)");
+   
 
-let arrayChars=nom.split("");
+    
+ for(let i=0;i<nom.length;i++){ 
+
+console.log(arrayMayus.indexOf(nom.charAt(i)));
+
+        if(arrayMayus.indexOf(nom.charAt(i))!=-1){
+            
+            alert(" HAY mayus");
+            nom=prompt("Ponga su nombre de usuario: (Debe tener minusculas y numeros)");
+
+        }
+
+   
+   
+    }
+ 
 
 
+
+
+
+ 
+
+
+
+/**/
+
+
+/*function encontrarMinus(booleanMinus){
 
     for(let i=0;i<arrayChars.length;i++){
-
-        if( arrayMayus.indexOf(arrayChars[i]) != -1 ){
-
-            alert("He encontrado un mayus");
-  
-        }if( arrayNums.indexOf(arrayChars[i]) == -1){
-
-                    alert("No he encontrado un numeo");
-                
-        }
-            nom=prompt("Ponga su nombre de usuario:(Debe tener minusculas y numeros)");
-            
+   
+        //si hay alguna minus en arrayChars boolean true
+        if(arrayMinus.indexOf(arrayChars[i])!=-1){  
+            booleanMinus==true;
         }
     
+     }
+    return booleanMinus;
+}
+function encontrarMinus(booleanMinus){
 
-
-
-let contrasenya=prompt("Ponga su contraseña:");
-
+    for(let i=0;i<arrayChars.length;i++){
+   
+        //si hay alguna minus en arrayChars boolean true
+        if(arrayMinus.indexOf(arrayChars[i])!=-1){  
+            booleanMinus==true;
+        }
+    
+     }
+    return booleanMinus;
+}
+*/
