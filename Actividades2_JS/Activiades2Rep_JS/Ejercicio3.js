660eee0bcd46c82a9191252cd37456a780cd8ed0
EriKoT <erik55@outlook.es>
2021-10-31 14:40:48 +0100

    31 oct 2021

--- Actividades2_JS/Activiades2Rep_JS/Ejercicio3.js
+++ Actividades2_JS/Activiades2Rep_JS/Ejercicio3.js
@@ -2,39 +2,24 @@ let random;
 
 const randoms = new Set();
 
-var uno=0;
-var dos=0;
-var tres=0;
-var cuatro=0;
-var cinco=0;
-var seis=0;
-
 // Tengo pensado si set me quita los valores repetidos meter cada uno de los valores
 // de la loteria del uno al seis para que no se reptian
 
 for(let i=1;i<=50;i++){
 
-    for(let j=0;j<50;j++){
+    document.write("<br>");
+    
+    for(let j=0;j<6;j++){
 
-        uno = Math.floor(Math.random()*49+1);
+        randoms.add(Math.floor(Math.random()*49+1));
         
-        dos = Math.floor(Math.random()*49+1);
+    };  
 
-        tres =Math.floor(Math.random()*49+1);
+    for(nums of randoms){
 
-        cuatro =Math.floor(Math.random()*49+1);
+        document.write(" "+nums);
 
-        cinco =Math.floor(Math.random()*49+1);
-
-        seis= Math.floor(Math.random()*49+1);
     }
-
-document.write("<br>"+randoms.add(uno)+" "+dos+" "+tres+" "+cuatro+" "+cinco+" "+seis);
-
+    randoms.clear();
 }
 
-/*for(uno of randoms){
-
-    document.write("<br>"+"- "+uno);
-
-}*/
---
+++ Actividades2_JS/Activiades2Rep_JS/Ejercicio4.js
@@ -0,0 +1,31 @@
+
+const randoms = new Map();
+
+for(let i =1;i<=10;i++){
+
+    randoms.set(i,0);
+
+}
+
+
+
+for(let j=1;j<=10000;j++){
+
+        random = Math.floor(Math.random()*10+1);
+
+    randoms.set(random,(randoms.get(random)+1));
+
+    
+}
+//Perfilar para que quede bonito
+for(nums of randoms){
+
+    document.write("<br>"+ nums);
+
+}
+
+
+// mostrar la suma de los numeros
+for(){
+
+}
