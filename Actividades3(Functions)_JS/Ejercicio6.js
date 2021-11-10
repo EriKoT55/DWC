c8b38c7553b686c07101e785c38e7bc136588232
erik <emendezmartin@iessonferrer.net>
2021-11-10 20:47:10 +0100

    10 nov 2021

--- Actividades3(Functions)_JS/Ejercicio6.js
+++ Actividades3(Functions)_JS/Ejercicio6.js
@@ -12,21 +12,29 @@ do{
 
 }while(seguir==true);
 //valores n1=1 n2=1, creo una variable que la igualo a n1
+
 function fibonacci(num){
-    let n1=1,n2=1;
+    
+    let n1=0,n2=1;
+
   for(let i=2;i<=num;i++){
 
     let f=n1;
     
     n1=n2;
-
+   
     n2=f+n2;
-
 
   }
 
 return n2;
 
 }
+//Me sale 1,1,1,2,3,5 / si pongo 5, deberia ser 0,1,1,2,3,5, mirar porque
+sucession=[];
+for(let i=0;i<=num;i++){
 
-document.write(fibonacci(num));
+sucession[i]=fibonacci(i);
+
+}
+document.write(sucession);
