c8cd337aa18ede75255d44d62e17ffde73485d15
EriKoT55 <erik55@outlook.es>
2021-11-19 20:22:23 +0100

    19 nov 2021

---
+++ Actividades4(POO)/Comunidad/Edificio.mjs
@@ -0,0 +1,14 @@
+class Edificio{
+
+    constructor(tipoVia,nombreVia,numeroEdificio,codigoPostal){
+
+        this.tipoVia=tipoVia;
+        this.nombreVia=nombreVia;
+        this.numeroEdificio=numeroEdificio;
+        this.codigoPostal=codigoPostal;
+
+    }
+
+    
+
+}
---
+++ Actividades4(POO)/Comunidad/ficheroJSON.mjs
@@ -0,0 +1,20 @@
+import {Edificio} from "./edificio.mjs";
+
+let edificio1 = `{
+    "tipoVia":"Calle",
+    "nombreVia":"García Prieto",
+    "numeroEdificio": "58A",
+    "codigoPostal": "07010",
+    "mapaPropietariosEdificio": {
+        "A": {
+            "1A": ["José Antonio López"],
+            "2A": ["Luisa Martínez"],
+            "3A": ["Marta Castellón", "José Montero"]
+        },
+        "B": {
+            "1B": [],
+            "2B": ["Antonio Pereira"],
+            "3B": []
+        }
+    }
+}`;
