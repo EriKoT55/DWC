c9d0a178f9397419351869d8418ace34aefd5aba
EriKoT55 <erik55@outlook.es>
2021-11-30 19:53:27 +0100

    30 nov 2021

--- Actividades4(POO)/URL'S/app.js
+++ Actividades4(POO)/URL'S/app.js
@@ -1,21 +1,27 @@
-let url;
-let seguir=true;
+
 /* https://www.w3schools.com/js/js_regexp.asp */
-url= prompt("Introduzca una URL para comprobarla: ");
+/* https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url */
+let url= prompt("Introduzca una URL para comprobarla: ");
 
 function comprobationUrl(url){
 
-    return /^(http|https|ftp)?::/+/["/"]{0,3}//*Shema*/
-    /[-A-ZÁÉÍÓÚÜÇa-záéíóúüç0-9]?//*que esten separadas un puntos*/
-    /::[-A-ZÁÉÍÓÚÜÇa-záéíóúüç0-9]?/
-    /@/
-    /:[0-90-9]/;
+    let comprobacion =/^(http|https|ftp)?::["/"]{0,3}(["-"][A-ZÁÉÍÓÚÜÇa-záéíóúüç0-9])*$/; //me falla el el guion
+   // /(/\-[A-ZÁÉÍÓÚÜÇa-záéíóúüç0-9])["."]{0,2}//*que esten separadas un puntos, no se si lo del punto esta bien*/
+   // /(/\:?\:?[A-ZÁÉÍÓÚÜÇa-záéíóúüç0-9])?//*contraseña*/
+   // /(/\@)?[A-ZÁÉÍÓÚÜÇa-záéíóúüç]["."][A-ZÁÉÍÓÚÜÇa-záéíóúüç]//*host*/
+   // /(/\:?\:?[0-90-9]?)^["/"]?[A-ZÁÉÍÓÚÜÇa-záéíóúüç]["/"]?[A-ZÁÉÍÓÚÜÇa-záéíóúüç]["/"]?//*Path*/
+   // /(/\?)([\W][\w])/;//esto es para el final del ej
+
+    if(comprobacion.test(url)==true){
+        return "t";
+    }else{
+        return "f";
+    }
 
 }
+document.write(comprobationUrl(url));
+/*do{
 
 
-do{
 
-
-
-}while();
+}while();*/
