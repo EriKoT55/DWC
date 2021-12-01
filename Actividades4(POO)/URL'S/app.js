a8876ecd28a5a222aaf18650779af367f779f181
EriKoT <erik55@outlook.es>
2021-12-01 14:46:15 +0100

    1 nov 2021

--- Actividades4(POO)/URL'S/app.js
+++ Actividades4(POO)/URL'S/app.js
@@ -5,19 +5,20 @@ let url= prompt("Introduzca una URL para comprobarla: ");
 
 function comprobationUrl(url){
 
-    let comprobacion =/^(http|https|ftp)?::["/"]{0,3}(["-"][A-ZÁÉÍÓÚÜÇa-záéíóúüç0-9])*$/; //me falla el el guion
-   // /(/\-[A-ZÁÉÍÓÚÜÇa-záéíóúüç0-9])["."]{0,2}//*que esten separadas un puntos, no se si lo del punto esta bien*/
-   // /(/\:?\:?[A-ZÁÉÍÓÚÜÇa-záéíóúüç0-9])?//*contraseña*/
+    let comprobacion =/^(http|https|ftp)?::["/"]{0,3}[-.A-ZÁÉÍÓÚÜÇa-záéíóúüç0-9]*\:{0,2}([\W]|[\w])\@$/;
+    //http::/P3p2-na.N0:Give|*100%@_-!@
    // /(/\@)?[A-ZÁÉÍÓÚÜÇa-záéíóúüç]["."][A-ZÁÉÍÓÚÜÇa-záéíóúüç]//*host*/
    // /(/\:?\:?[0-90-9]?)^["/"]?[A-ZÁÉÍÓÚÜÇa-záéíóúüç]["/"]?[A-ZÁÉÍÓÚÜÇa-záéíóúüç]["/"]?//*Path*/
    // /(/\?)([\W][\w])/;//esto es para el final del ej
 
-    if(comprobacion.test(url)==true){
-        return "t";
-    }else{
-        return "f";
-    }
-
+  
+        if(comprobacion.test(url)==true){
+            return "t";
+        }else{
+            
+            return "f";
+        }
+   
 }
 document.write(comprobationUrl(url));
 /*do{
