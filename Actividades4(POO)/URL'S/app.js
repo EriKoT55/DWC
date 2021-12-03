1272550d20b492d5ca759a0de5e78271366488a4
EriKoT55 <erik55@outlook.es>
2021-12-03 18:52:37 +0100

    3 dic 2021

--- Actividades4(POO)/URL'S/app.js
+++ Actividades4(POO)/URL'S/app.js
@@ -4,7 +4,7 @@
 let url= prompt("Introduzca una URL para comprobarla: ");
 
 function comprobationUrl(url){
-                                                                                                                                        //Mirar esta parte al poner * + ? fuera del parentesis peta y creo que cualquier cosa que pongas dara true
+                                                                                                                                        
     let comprobacion =/^(http|https|ftp)?:\/{0,3}([A-ZÇa-zç0-9\-\.]*:.+|[A-ZÇa-zç0-9\-\.]*)\@[A-ZÇa-zç0-9\-]+\.[A-ZÇa-zç0-9\-]+(:[0-9]{1,5})?(\/.+\/?)*.+\?$/;
     //http:/P3p2-na.N0:Give|*100%@_-!@
     //http:/P3p2-na.N0:Give|*100%@_-!@MAn3L0.com:80/Give|*100%@_-!@/
