f0703639b10d2d782a5b4d7bfede5b45d6a116ce
EriKoT55 <erik55@outlook.es>
2022-01-10 19:10:12 +0100

    10 enero 2022

---
+++ Actividades5_JS/eliminar_Valores_repetidos/js.js
@@ -0,0 +1,32 @@
+var randomNums;
+var randomBalls;
+
+const NUMS_SIN_REPETIR = new Set();
+
+randomBalls=Math.floor(Math.random()*10 +5);
+randomNums=Math.floor(Math.random()*9 + 10102);
+
+document.write("<br>");
+document.write("<h3>Entre estas "+randomBalls+" bolas... </h3><br>");
+document.write("<div>");
+for(let i=0;i<randomBalls;i++){
+    randomNums=Math.floor(Math.random()*9 + 10102);
+    document.write("<h1>&#"+randomNums+"</h1>");
+    
+    NUMS_SIN_REPETIR.add(randomNums);
+
+}
+document.write("</div>");
+document.write("<br>");
+document.write("<br>");
+
+document.write("<h3>Hay "+NUMS_SIN_REPETIR.size+" bolas distintas</h3>");
+
+document.write("<br>");
+document.write("<br>");
+document.write("<div>");
+    for(x of NUMS_SIN_REPETIR){
+        document.write("<h1 class='h11'>&#"+x+"</h1>");
+    }
+document.write("</div>");
+
---
+++ Actividades5_JS/joc_piedra_papel_tijera/js.js
@@ -0,0 +1,20 @@
+let randomPosc;
+
+let j1=["&#129308","&#129306","&#128406"];
+let j2=["&#129307","&#129306","&#128406"];
+
+randomj1=Math.floor(Math.random()*3);
+randomj2=Math.floor(Math.random()*3);
+
+document.write("j1 j2 <br>");
+    document.write(j1[randomj1]);
+    document.write(j2[randomj2]);
+
+if(j1[randomj1]==j2[randomj2] || j1[randomj1]=="&#129308" && j2[randomj2]=="&#129307"){
+    document.write("<br> empate");
+}else if(j1[randomj1]=="&#129308" && j2[randomj2]=="&#128406" || j1[randomj1]=="&#128406" && j2[randomj2]=="&#129306" || j1[randomj1]=="&#129306" && j2[randomj2]=="&#129307"){
+    document.write("<br> j1 gana");
+}else if(j1[randomj1]=="&#128406" && j2[randomj2]=="&#129307" || j1[randomj1]=="&#129306" && j2[randomj2]=="&#128406" || j1[randomj1]=="&#129307" && j2[randomj2]=="&#129306"){
+    document.write("<br> j2 gana");
+}
+
