72b97a4824baf47c80964e1c16a4266e71eb1f87
EriKoT55 <erik55@outlook.es>
2022-01-17 17:56:34 +0100

    17 ENERO 2022

--- Actividades6_DOM/Ejercicio1/ejercicio1.js
+++ Actividades6_DOM/Ejercicio1/ejercicio1.js
@@ -1,19 +1,70 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    //CREACION DE VARIABLES
-    let numP = document.getElementsByTagName("p");
+
+    /* Creo el nodo P y el NodoText hago que contenido sea hijo de paragrafo 
+     * y despues lo muestro siendo paragrafo hijo del div(info) */
+    // PARA MOSTRAR CONTENIDO EN EL DIV
+    // a
     let numA = document.getElementsByTagName("a");
 
-    /* Creo el nodo P y el nodo Text hago que contenido sea hijo de paragrafo 
-     * y despues lo muestro */
-    let paragrafo = document.createElement("p");
-    let contenido = document.createTextNode("Numero de paragrafos: " + numP.length);
+    let paragrafo1 = document.createElement("p");
+    let contenidoL = document.createTextNode("Numero de links: " + numA.length);
 
-    paragrafo.appendChild(contenido);
+    paragrafo1.appendChild(contenidoL);
 
-    document.getElementById("info").appendChild(paragrafo);
-    /******************************/
+    document.getElementById("info").appendChild(paragrafo1);
 
-    document.getElementById("info").innerHTML="<p>"+numA.length+"</p>";
- 
+    /***********************************************************************/
+
+    // p
+    let numP = document.getElementsByTagName("p");
+
+    let paragrafo2 = document.createElement("p");
+    let contenidoP = document.createTextNode("Numero de paragrafos: " + numP.length);
+
+    paragrafo2.appendChild(contenidoP);
+
+    document.getElementById("info").appendChild(paragrafo2);
+
+    /***********************************************************************/
+
+    // penultimo a
+    let penultimo = numA.length-2;
+    let penultimoA = numA[penultimo];
+
+    let paragrafo3 = document.createElement("p");
+    let contenido_penult_a = document.createTextNode("El penúltimo enlace apunta a: " + penultimoA);
+
+    paragrafo3.appendChild(contenido_penult_a);
+
+    document.getElementById("info").appendChild(paragrafo3);
+
+/***********************************************************************/
+
+    // ultimo a
+    let ultimo = numA.length-1;
+    let ultimoA = numA[ultimo];
+
+    let paragrafo4 = document.createElement("p");
+    let contenido_ult_a = document.createTextNode("El último enlace apunta a: " + ultimoA);
+
+    paragrafo4.appendChild(contenido_ult_a);
+
+    document.getElementById("info").appendChild(paragrafo4);
+
+/***********************************************************************/    
+    /* NO FUNCIONA */
+    // links a http://prueba/
+
+    let linksPrueba=numA["http://prueba/"]
+
+    let paragrafo5 = document.createElement("p");
+    let contenidoLinkPrueba = document.createTextNode("El último enlace apunta a: " + linksPrueba.length);
+
+    paragrafo5.appendChild(contenidoLinkPrueba);
+
+    document.getElementById("info").appendChild(paragrafo5);
+
+    /*document.getElementById("info").innerHTML="<p>"+numA.length+"</p>";*/
+
 });
