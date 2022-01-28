5000c6433a4d9c816ca537ec46d32fa73c8f35a9
EriKoT55 <erik55@outlook.es>
2022-01-28 21:00:40 +0100

    28 enero 22022

---
+++ Actividades6_DOM/Ejercicio4Form/ejercicio4.js
@@ -0,0 +1,21 @@
+function habilitarSelect(select) {
+    /*habilita quan el checkbox on es troba sigui seleccionat i ha de deshabilitar el
+    select si el checkbox es deshabilita. */
+
+    let select = document.getElementById(select);
+
+    if () {
+        select.disabled=false;
+    } else {
+        select.disabled=true;
+    }
+
+
+}
+
+function retornaValor(objForm) {
+
+
+
+
+}
