322bd2ff0aad2fb4fc3cfe2784f86447f197c6ce
EriKoT55 <erik55@outlook.es>
2022-01-31 19:16:17 +0100

    31 enero 2022

--- Actividades6_DOM/Ejercicio4Form/ejercicio4.js
+++ Actividades6_DOM/Ejercicio4Form/ejercicio4.js
@@ -1,21 +1,30 @@
-function habilitarSelect(select) {
+
+function habilitarSelect(selectID) {
     /*habilita quan el checkbox on es troba sigui seleccionat i ha de deshabilitar el
     select si el checkbox es deshabilita. */
 
-    let select = document.getElementById(select);
+    let select = document.getElementById(selectID);
 
-    if () {
+    //console.log(selectMarcaBici);
+
+    if (select.disabled) {
         select.disabled=false;
     } else {
         select.disabled=true;
     }
 
-
 }
 
-function retornaValor(objForm) {
 
 
+function retornaValor() {
 
-
-}
+    let form = document.getElementById('nombre');
+     console.log(form.value);
+ 
+ }
+ 
+ 
+ function validarFormulario(formID){
+     retornaValor();
+ }
