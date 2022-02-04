b42ec0e67ee6f082cd590d787e99412c8fee5bc9
EriKoT55 <erik55@outlook.es>
2022-02-04 20:36:25 +0100

    4 febrero 2022

--- Actividades6_DOM/Ejercicio4Form/ejercicio4.js
+++ Actividades6_DOM/Ejercicio4Form/ejercicio4.js
@@ -14,12 +14,65 @@ function habilitarSelect(selectID) {
 
 }
 
+const elementrosFunciones = {"text":"text",
+    "textarea":"text",
+    "checkbox":"check",
+    "readio":"check",
+    "select-multiple":"select-multiple",
+    "select-one":"select-one"
+
+}
+
+const funcionesRetornar={
+
+    'text':function(elementoForm){
+        return elementoForm.value;
+    }
+    ,
+    'check':function(elementoForm){
+        let valor="";
+        if(elementoForm.checked){
+             
+            valor = elementoForm.value;
+            return valor;
+        }
+    }
+    ,
+    'select-multiple': function(elementoForm){
+        let arraySelects = [];
+
+        for(let i=0; i<elementoForm.options.length;i++){
+
+            if(elementoForm.options.selected==true){
+                arraySelects.push(elementoForm.options.value);
+            }
+        }
+
+        return arraySelects;
+
+    }
+    ,
+    "select-one":function (elementForm){
+        let valor="";
+        let indexSelect= elementForm.selectedIndex;
+
+       valor= elementForm.options[indexSelect].value;
+       return valor;
+    }
+
+};
+
 
 
 function retornaValor(elementForm) {
 
     let tipo = elementForm.type;
 
+  
+   return  funcionesRetornar[elementForm[tipo]](elementForm);
+
+    /*
+      let valor="";
     if (tipo == "text" || tipo == "textarea") {
         valor = elementForm.value;
     } else if (tipo == "checkbox" || tipo == "radio") {
@@ -35,18 +88,17 @@ function retornaValor(elementForm) {
             if(elementForm.options.selected==true){
                 valor=arraySelects.push(elementForm.options.value);
             }
+        }
 
-        elementForm.options[];
-
-    }else if(tipo="select-one"){
+    }else if(tipo=="select-one"){
 
         let indexSelect= elementForm.selectedIndex;
 
-        elementForm.options[indexSelect];
+        valor=elementForm.options[indexSelect].value;
 
     }
 
-    return valor;
+    return valor;*/
 
 }
 
@@ -54,11 +106,12 @@ function retornaValor(elementForm) {
 function validarFormulario(formID) {
 
     let form = document.getElementById(formID);
+    let muestraPantalla="Resultado de la validacion: ";
 
     for (let elemento of form.elements) {
 
         retornaValor(elemento);
-
+        muestraPantalla+=;
     }
 
 }
