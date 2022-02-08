ed57b1ff0aa6095636613dd6132fbcfa60135416
EriKoT55 <erik55@outlook.es>
2022-02-08 19:26:02 +0100

    8 feb 2022

---
+++ Actividades6_DOM/MendezErik_TaulesDOM/practica.js
@@ -0,0 +1,40 @@
+function crearTablaColores(tablaColores,numColores){
+
+let elementTableID = document.getElementById(tablaColores);
+
+
+
+}
+
+function crearTabla(numFila,numColumna){
+
+
+    let tBodyElement= document.createElement("tbody");
+
+    let trElement=document.createElement("tr");
+
+    let tdElement = document.createElement("td");
+    
+    //Creo que sera algo así
+    let contenidoTD= document.createTextNode(introducirDatosTabla());
+
+}
+
+function introducirDatosTabla(tabla,datos){
+
+
+
+}
+
+function permutarFilas(tablaColoresm,fila1,fila2){
+
+
+
+}
+
+
+function cambiarFondo(tablaColores,filaFondo){
+
+
+
+}
