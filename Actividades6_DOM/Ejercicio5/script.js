ac69e8a2561f59066901af354d546be94363b640
EriKoT55 <erik55@outlook.es>
2022-02-16 20:36:12 +0100

    16 febrero 2022

---
+++ Actividades6_DOM/Ejercicio5/script.js
@@ -0,0 +1,26 @@
+function invertir() {
+
+    //Conversion a array con los tres puntos
+    //con el operador de propagacion, paso el node list a array
+    let p = [...document.getElementsByTagName("p")];
+
+
+
+  /*  for (let i = 0; i < body[0].getElementsByTagName("p").length; i++) {
+
+        p[i] = body[0].getElementsByTagName("p")[i];
+        //console.log(body[0].getElementsByTagName("p").length);
+
+    }*/
+
+    p.reverse();
+
+    for (let i = 0; i < p.length; i++) {
+
+        document.body.appendChild(p[i]);
+        //console.log(body[0].getElementsByTagName("p").length);
+
+    }
+
+
+}
